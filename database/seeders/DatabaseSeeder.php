a95e929250c1441170bef01dc53605c8abe1a2a3
amemske <olywave@gmail.com>
2022-09-29 13:09:59 +0300

    eloquent models, design conversion to blade templates, setting up dummy data using factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      //create 1 user only, don't generate fake data
+      $user = User::factory()->create([
+         'name' => 'John Doe'
+      ]);
+
+      Post::factory(5)->create([
+         'user_id' => $user->id //associate this with a single user
+      ]); 
+      
+      //Post::factory(5)->create() this will create 5 user, 5 categories, 5 posts
+
+
+
+      //   User::truncate();
+      //   Category::truncate();
+      //   Post::truncate();
+
+        // $user = User::factory()->create();
+
+      //   $personal = Category::create([
+      //       'name' => 'Personal',
+      //       'slug' => 'personal'
+      //    ]);
+      //    $family = Category::create([
+      //       'name' => 'Family',
+      //       'slug' => 'family'
+      //    ]);
+         
+      //    $work = Category::create([
+      //       'name' => 'Work',
+      //       'slug' => 'work'
+      //    ]);
+
+      //    Post::create([
+      //       'user_id' =>$user->id,
+      //       'category_id' => $family->id,
+      //       'title' =>'My Family Post',
+      //       'slug' => 'my-family-post',
+      //       'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      //       'body' => 'Blanditiis labore praesentium illum unde neque, quis expedita maiores vel saepe reprehenderit quidem id aliquid quod dolorum deleniti laboriosam adipisci officiis ea.'
+      //    ]);
+
+      //    Post::create([
+      //       'user_id' =>$user->id,
+      //       'category_id' => $work->id,
+      //       'title' =>'My Work Post',
+      //       'slug' => 'my-work-post',
+      //       'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      //       'body' => 'Blanditiis labore praesentium illum unde neque, quis expedita maiores vel saepe reprehenderit quidem id aliquid quod dolorum deleniti laboriosam adipisci officiis ea.'
+      //    ]);
+
     }
 }
